b2856fd2b89f8ad8c11a7db232ed35f180f918b6
myisha <myisha@gmail.com>
2026-06-17 14:34:17 +0530

    EKS Terraform and ArgoCD setup

---
+++ eks/main.tf
@@ -0,0 +1,95 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# =========================
+# VPC
+# =========================
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs = [
+    "ap-south-1a",
+    "ap-south-1b"
+  ]
+
+  public_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  private_subnets = [
+    "10.0.3.0/24",
+    "10.0.4.0/24"
+  ]
+
+  enable_nat_gateway = false
+  single_nat_gateway = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  # IMPORTANT: Required for worker nodes in public subnets
+  map_public_ip_on_launch = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  tags = {
+    Environment = "lab"
+    Terraform   = "true"
+  }
+}
+
+# =========================
+# EKS
+# =========================
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.11.0"
+
+  cluster_name = "eks-cluster1"
+
+  # Let AWS choose supported version
+  # Do NOT set cluster_version
+
+  cluster_endpoint_public_access = true
+
+  vpc_id = module.vpc.vpc_id
+
+  subnet_ids = module.vpc.public_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.small"]
+
+      min_size     = 2
+      max_size     = 3
+      desired_size = 2
+      
+    }
+  }
+
+  tags = {
+    Environment = "lab"
+    Terraform   = "true"
+  }
+}
